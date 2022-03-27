b2bfda83311cf2ec0b7c17da38724aabeab36208
Jake Archibald <jaffathecake@gmail.com>
2022-03-27 13:12:22 +0100

    Back icon

--- src/shared/general/HeaderLayout/index.tsx
+++ src/shared/general/HeaderLayout/index.tsx
@@ -7,12 +7,14 @@ import 'add-css:./styles.module.css';
 interface Props {
   onHomeClick?: (event: Event) => void;
   scrollKey?: unknown;
+  showBackIcon?: boolean;
 }
 
 const Header: FunctionalComponent<Props> = ({
   onHomeClick,
   children,
   scrollKey,
+  showBackIcon,
 }: RenderableProps<Props>) => {
   const scrollerRef = createRef<HTMLDivElement>();
 
@@ -22,12 +24,23 @@ const Header: FunctionalComponent<Props> = ({
 
   return (
     <div class={styles.mainLayout}>
-      <header class={styles.header}>
+      <header
+        class={[
+          styles.header,
+          showBackIcon && styles.showBackIcon,
+          'site-header',
+        ]
+          .filter(Boolean)
+          .join(' ')}
+      >
         <a
           href="/"
           class={styles.homeLink}
           onClick={(event) => onHomeClick?.(event)}
         >
+          <svg class={styles.backIcon} viewBox="0 0 24 24">
+            <path d="M20 11H7.8l5.6-5.6L12 4l-8 8 8 8 1.4-1.4L7.8 13H20v-2z" />
+          </svg>
           HTTP 203
         </a>
       </header>
