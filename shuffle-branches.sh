cd90157e0c83ff2411eb01d926b9c931e1a9a204
Jake Archibald <jaffathecake@gmail.com>
2022-04-01 16:30:12 +0100

    Update branches

--- shuffle-branches.sh
+++ shuffle-branches.sh
@@ -7,7 +7,7 @@ git checkout fixed-header
 git rebase --onto talk-slide-from-side origin/talk-slide-from-side
 git checkout header-bug
 git rebase --onto fixed-header origin/fixed-header
-git checkout buggy-side-bar
+git checkout steadier-desktop-transition
 git rebase --onto header-bug origin/header-bug
 git checkout video-transition
-git rebase --onto buggy-side-bar origin/buggy-side-bar
+git rebase --onto steadier-desktop-transition origin/steadier-desktop-transition
