9dc9b51727c203704eb9952468dba193d014a095
jakearchibald <jaffathecake@gmail.com>
2022-10-31 12:36:45 +0000

    Move back to old cohost switch for now

--- src/shared/Index/CohostSwitch/index.tsx
+++ src/shared/Index/CohostSwitch/index.tsx
@@ -1,10 +1,9 @@
-import { h, FunctionalComponent, RenderableProps, Fragment } from 'preact';
-import { useEffect, useRef, useState } from 'preact/hooks';
+import { h, FunctionalComponent, RenderableProps } from 'preact';
+import {} from 'preact/hooks';
 import { cohosts } from 'shared/data';
 
 import * as styles from './styles.module.css';
 import 'add-css:./styles.module.css';
-import { usePageTransition } from 'shared/utils';
 
 interface Props {
   selectedCohost?: typeof cohosts[number];
@@ -13,91 +12,17 @@ interface Props {
 const CohostSwitch: FunctionalComponent<Props> = ({
   selectedCohost,
 }: RenderableProps<Props>) => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const dialogRef = useRef<HTMLDialogElement>(null);
-
-  const startDialogOpenTransition = usePageTransition({
-    incoming() {
-      document.documentElement.classList.add('no-root-transition');
-    },
-    done() {
-      document.documentElement.classList.remove('no-root-transition');
-    },
-  });
-
-  const startDialogCloseTransition = usePageTransition({
-    incoming() {
-      document.documentElement.classList.add('no-root-transition');
-    },
-    done() {
-      document.documentElement.classList.remove('no-root-transition');
-    },
-  });
-
-  useEffect(() => {
-    const listener = () => setModalOpen(false);
-    const dialog = dialogRef.current!;
-    dialog.addEventListener('close', listener);
-    return () => dialog.removeEventListener('close', listener);
-  }, [dialogRef]);
-
-  useEffect(() => {
-    const listener = (event: Event) => {
-      event.preventDefault();
-      console.log('close from cancel');
-      void startDialogCloseTransition().then(() => {
-        dialogRef.current!.close();
-      });
-    };
-    const dialog = dialogRef.current!;
-    dialog.addEventListener('cancel', listener);
-    return () => dialog.removeEventListener('cancel', listener);
-  }, [dialogRef]);
-
-  useEffect(() => {
-    if (!dialogRef.current?.open) return;
-    console.log('close from cohost change');
-
-    void startDialogCloseTransition().then(() => {
-      dialogRef.current!.close();
-    });
-  }, [selectedCohost, startDialogCloseTransition]);
-
-  const onButtonClick = () => {
-    void startDialogOpenTransition().then(() => {
-      dialogRef.current!.showModal();
-      setModalOpen(true);
-    });
-  };
-
   return (
-    <>
-      <button
-        class={styles.cohostSwitch}
-        style={{ visibility: modalOpen ? 'hidden' : '' }}
-        onClick={onButtonClick}
-      >
-        Cohost:{' '}
-        <span class={styles.selectedCohost}>{selectedCohost || 'All'}</span>
-      </button>
-      <dialog ref={dialogRef} class={styles.modal}>
-        <ol class={styles.cohostList}>
-          <li>
-            <a href="/">All</a>
-          </li>
-          {cohosts.map((cohost) => (
-            <li>
-              <a
-                href={`/with-${cohost.toLowerCase()}/`}
-                class={selectedCohost === cohost ? styles.currentInList : ''}
-              >
-                {cohost}
-              </a>
-            </li>
-          ))}
-        </ol>
-      </dialog>
-    </>
+    <ol class={[styles.cohostSwitch, 'cohost-switch'].join(' ')}>
+      <li class={selectedCohost ? '' : styles.currentCohost}>
+        <a href="/">All</a>
+      </li>
+      {cohosts.map((cohost) => (
+        <li class={selectedCohost === cohost ? styles.currentCohost : ''}>
+          <a href={`/with-${cohost.toLowerCase()}/`}>{cohost}</a>
+        </li>
+      ))}
+    </ol>
   );
 };
 
