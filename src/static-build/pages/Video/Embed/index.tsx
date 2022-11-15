21dc4f88afbefdd2b3034ce3e7c3a8809e34c50a
jakearchibald <jaffathecake@gmail.com>
2022-11-15 15:53:40 +0000

    Fix iframe order

--- src/static-build/pages/Video/Embed/index.tsx
+++ src/static-build/pages/Video/Embed/index.tsx
@@ -12,7 +12,15 @@ const Embed: FunctionalComponent<Props> = ({
   video,
 }: RenderableProps<Props>) => {
   return (
-    <div class={styles.embedContainer}>
+    <div class={[styles.embedContainer, 'embed-container'].join(' ')}>
+      <img
+        class={styles.videoImg}
+        srcset={ytSrcset(video.id)}
+        alt={video.title}
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        fetchpriority="high"
+      />
       <iframe
         class={styles.embed}
         width="560"
@@ -22,14 +30,6 @@ const Embed: FunctionalComponent<Props> = ({
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
-      />
-      <img
-        class={styles.videoImg}
-        srcset={ytSrcset(video.id)}
-        alt={video.title}
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        fetchpriority="high"
       />
     </div>
   );
